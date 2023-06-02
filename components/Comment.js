ead167126a5215d3574fd4972152b8c6426556ed
wangqy <wangqy@inketang.com>
2023-06-02 17:52:36 +0800

    去除滚动条

--- components/Comment.js
+++ components/Comment.js
@@ -91,7 +91,7 @@ const Comment = ({ frontMatter }) => {
 
         {BLOG.COMMENT_GISCUS_REPO && (
           <div key="Giscus">
-            <GiscusComponent className="px-2 overflow-y-scroll scrollbar-none" />
+            <GiscusComponent className="px-2" />
           </div>
         )}
 
--- themes/hexo/LayoutSlug.js
+++ themes/hexo/LayoutSlug.js
@@ -80,7 +80,7 @@ export const LayoutSlug = props => {
           <div className='pt-4 border-dashed'></div>
 
           {/* 评论互动 */}
-          <div className="duration-200 overflow-x-auto bg-white dark:bg-hexo-black-gray px-3">
+          <div className="duration-200 bg-white dark:bg-hexo-black-gray px-3">
             <Comment frontMatter={post} />
           </div>
         </div>}
